62320995c2c0044de4b2a9cbd8e5002a6be46bdb
springruse <152939330+springruse@users.noreply.github.com>
2025-07-01 10:28:43 -0600

    deleted game

--- Source/Engine/Core/Math.h
+++ Source/Engine/Core/Math.h
@@ -1,6 +1,6 @@
 #pragma once
 
-namespace death {
+namespace piMath {
 	constexpr float pi = 3.14159265358979323846f; // Pi constant
 	constexpr float two_pi = 6.28318530717958647692f; // 2 * Pi constant
 	constexpr float half_pi = 1.57079632679489661923f; // Pi / 2 constant
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,12 +1,13 @@
+#define NAME "Alec"
 #include "Core/Math.h"
 #include <iostream>
-#define NAME "Alec"
+
 
 int main() {
 
 	std::cout << NAME;
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "I am writing this to future me!" << std::endl;
-	std::cout << death::pi << std::endl;
+	std::cout << piMath::pi << std::endl;
 
 }
