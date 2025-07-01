308e73db2fd9583fa94fa970ab2d32012f3d5437
springruse <152939330+springruse@users.noreply.github.com>
2025-07-01 10:16:32 -0600

    added include core direct and alec

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,8 +1,12 @@
+#include "Core/Math.h"
 #include <iostream>
+#define NAME "Alec"
 
 int main() {
 
+	std::cout << NAME;
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "I am writing this to future me!" << std::endl;
+	std::cout << death::pi << std::endl;
 
 }
